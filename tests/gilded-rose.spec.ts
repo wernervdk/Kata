55540baae95da3e926317ecfe1bbbbdc790ec6ae
Werner Van der Kuylen <werner@gotoweb.be>
2021-02-18 14:54:01 +0100

    fix: fixed failing test

--- tests/gilded-rose.spec.ts
+++ tests/gilded-rose.spec.ts
@@ -2,9 +2,10 @@ import { Item, GildedRose } from "../app/gilded-rose";
 
 describe("Gilded Rose", () => {
   it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+    const result = 'foo';
+    const gildedRose = new GildedRose([new Item(result, 0, 0)]);
     const items = gildedRose.updateQuality();
 
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe(result);
   });
 });
