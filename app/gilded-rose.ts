86f16c82f0dcf30d2f3678f6004381404dcf9f33
Werner Van der Kuylen <werner@gotoweb.be>
2021-02-18 15:53:41 +0100

    fix: added refactoring with extra test for Conjured

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,23 +1,153 @@
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
+  name: string;
+  sellIn: number;
+  quality: number;
 
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 }
 
+// - "Aged Brie" actually increases in Quality the older it gets
+// - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+// - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+// - "Conjured" items degrade in Quality twice as fast as normal items
+// - Once the sell by date has passed, Quality degrades twice as fast
+// - The Quality of an item is never negative
+// - The Quality of an item is never more than 50
+
 export class GildedRose {
-    items: Array<Item>;
+  items: Array<Item>; //can be made private readonly by cannot change by design
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
+  constructor(items: Array<Item>) {
+    this.items = items.slice(0);
+  }
 
-    updateQuality() {
+  updateQuality(): Array<Item> {
+    const MaxEntries = 50;
+    const minEntries = 0;
+    const AgedBrie = "Aged Brie";
+    const Sulfuras = "Sulfuras";
+    const BackstagePasses = "Backstage passes";
+    const Conjured = "Conjured";
+
+    const setMinAndMaxRanges = (itm: Item): Item => {
+      if (itm.quality > MaxEntries) {
+        itm.quality = MaxEntries;
+      }
+
+      if (itm.sellIn < minEntries) {
+        itm.sellIn = minEntries;
+      }
+
+      return itm;
+    };
+
+    const handleConjured = (itm: Item): Item => {
+      if (!itm.name.startsWith(Conjured)) {
+        throw new Error(
+          `itm must starts with '${Conjured}' but is ${itm.name}`
+        );
+      }
+
+      itm.quality = itm.quality - 2;
+      if (itm.quality > 0) {
+        itm.sellIn = itm.sellIn - 2;
+      }
+      if (itm.sellIn < 0) {
+        itm.quality = itm.quality + 2;
+      }
+
+      itm = setMinAndMaxRanges(itm);
+
+      return itm;
+    };
+
+    const handlePagedBrie = (itm: Item): Item => {
+      if (!itm.name.startsWith(AgedBrie)) {
+        throw new Error(`itm must start with '${AgedBrie}' but is ${itm.name}`);
+      }
+
+      itm.quality = itm.quality + 1;
+      itm.sellIn = itm.sellIn - 1;
+      if (itm.sellIn < 0) {
+        itm.quality = itm.quality + 1;
+      }
+
+      itm = setMinAndMaxRanges(itm);
+
+      return itm;
+    };
+
+    const handleBackstagePasses = (itm: Item): Item => {
+      if (!itm.name.startsWith(BackstagePasses)) {
+        throw new Error(
+          `itm must start with '${BackstagePasses}' but is ${itm.name}`
+        );
+      }
+
+      itm.quality = itm.quality + 1;
+      if (itm.sellIn < 11) {
+        itm.quality = itm.quality + 1;
+      }
+      if (itm.sellIn < 6) {
+        itm.quality = itm.quality + 1;
+      }
+      itm.sellIn = itm.sellIn - 1;
+
+      itm = setMinAndMaxRanges(itm);
+
+      return itm;
+    };
+
+    const handleSulfuras = (itm: Item): Item => {
+      if (!itm.name.startsWith(Sulfuras)) {
+        throw new Error(`itm must start with '${Sulfuras}' but is ${itm.name}`);
+      }
+      if (itm.quality !== 80) {
+        itm.quality = 80;
+      }
+
+      return itm;
+    };
+
+    const HandleDefault = (itm: Item): Item => {
+      itm.quality = itm.quality - 1;
+      if (itm.quality > 0) {
+        itm.sellIn = itm.sellIn - 1;
+      }
+      if (itm.sellIn < 0) {
+        itm.quality = itm.quality + 1;
+      }
+
+      itm = setMinAndMaxRanges(itm);
+
+      return itm;
+    };
+
+    const handleItem = (itm: Item): Item => {
+      if (itm.name.startsWith(AgedBrie)) {
+        itm = handlePagedBrie(itm);
+      } else if (itm.name.startsWith(BackstagePasses)) {
+        itm = handleBackstagePasses(itm);
+      } else if (itm.name.startsWith(Conjured)) {
+        itm = handleConjured(itm);
+      } else if (itm.name.startsWith(Sulfuras)) {
+        itm = handleSulfuras(itm);
+      } else {
+        itm = HandleDefault(itm);
+      }
+
+      return itm;
+    };
+
+    this.items.forEach((itm) => {
+      itm = handleItem(itm);
+    });
+
+    /*
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
@@ -63,7 +193,8 @@ export class GildedRose {
                 }
             }
         }
+*/
 
-        return this.items;
-    }
+    return this.items;
+  }
 }
--- tests/gilded-rose.spec.ts
+++ tests/gilded-rose.spec.ts
@@ -2,69 +2,75 @@ import { Item, GildedRose } from "../app/gilded-rose";
 
 describe("Gilded Rose", () => {
   it("should foo", () => {
-    const result = 'foo';
+    const result = "foo";
     const gildedRose = new GildedRose([new Item(result, 0, 0)]);
     const items = gildedRose.updateQuality();
 
     expect(items[0].name).toBe(result);
   });
 
-  describe("should create baseline test for Aged Brie", () => {
-    const items = [
-      new Item('Aged Brie', 2, 0),
-    ]
+  it("should create baseline test for Aged Brie", () => {
+    const items = [new Item("Aged Brie", 2, 0)];
 
-    const result =new GildedRose(items).updateQuality()
-
-    expect(result).toMatchObject([{name: 'Aged Brie', sellIn: 1, quality: 1}])
-
-  })
-
-  describe("should create baseline test for Sulfuras", () => {
-    const items = [
-      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-      new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    ]
-
-    const result =new GildedRose(items).updateQuality()
+    const result = new GildedRose(items).updateQuality();
 
     expect(result).toMatchObject([
-      {name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 80 },
-      {name: 'Sulfuras, Hand of Ragnaros', sellIn: -1, quality: 80 },
-    ])
+      { name: "Aged Brie", sellIn: 1, quality: 1 },
+    ]);
+  });
 
-  })
-
-  describe("should create baseline test for Backstage passes", () => {
+  it("should create baseline test for Sulfuras", () => {
     const items = [
-      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    ]
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    ];
 
-    const result =new GildedRose(items).updateQuality()
+    const result = new GildedRose(items).updateQuality();
 
     expect(result).toMatchObject([
-      new Item('Backstage passes to a TAFKAL80ETC concert', 14, 21),
-      new Item('Backstage passes to a TAFKAL80ETC concert', 9, 50),
-      new Item('Backstage passes to a TAFKAL80ETC concert', 4, 50),
-    ])
+      { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
+      { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
+    ]);
+  });
 
-  })
-
-  describe("should create baseline test for the other items", () => {
+  it("should create baseline test for Backstage passes", () => {
     const items = [
-      new Item('+5 Dexterity Vest', 10, 20),
-      new Item('Elixir of the Mongoose', 5, 7),
-    ]
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    ];
 
-    const result =new GildedRose(items).updateQuality()
+    const result = new GildedRose(items).updateQuality();
+    console.log(result);
 
     expect(result).toMatchObject([
-      new Item('+5 Dexterity Vest', 9, 19),
-      new Item('Elixir of the Mongoose', 4, 6),
-    ])
+      new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+    ]);
+  });
 
-  })
+  it("should create baseline test for the other items", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Elixir of the Mongoose", 5, 7),
+    ];
 
+    const result = new GildedRose(items).updateQuality();
+
+    expect(result).toMatchObject([
+      new Item("+5 Dexterity Vest", 9, 19),
+      new Item("Elixir of the Mongoose", 4, 6),
+    ]);
+  });
+
+  it("should create baseline test for Conjured", () => {
+    const items = [new Item("Conjured Mana Cake", 3, 6)];
+
+    const result = new GildedRose(items).updateQuality();
+
+    expect(result).toMatchObject([
+      { name: "Conjured Mana Cake", sellIn: 1, quality: 4 },
+    ]);
+  });
 });
